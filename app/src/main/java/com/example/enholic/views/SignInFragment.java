190ebec5c90677809ea7d44e849311a37a3b5698
Gia Quyen <giaquyen200174@gmail.com>
2022-01-07 14:53:40 +0700

    User need to login after close app

--- app/src/main/java/com/example/enholic/views/SignInFragment.java
+++ app/src/main/java/com/example/enholic/views/SignInFragment.java
@@ -42,11 +42,6 @@ public class SignInFragment extends Fragment {
         viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication())).get(AuthViewModel.class);
         navController = Navigation.findNavController(view);
 
-        //if the user has logged in before
-        if (viewModel.getCurrentUser() != null){
-            navController.navigate(R.id.registeredHomepageFragment);
-        }
-
         editEmail = view.findViewById(R.id.inputEmail_SignIn);
         editPass = view.findViewById(R.id.inputPassword_SignIn);
         signUpBtn = view.findViewById(R.id.registerBtn_SignIn);
